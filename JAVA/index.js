e36a4b8315ea87668a0073b46bd2983d7a247aa8
Skie <skielynn.1105945@gmail.com>
2022-11-03 20:17:43 -0700

    testing step

--- JAVA/index.js
+++ JAVA/index.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+var canvas;
+var ctx;
+
+var gameLoop;
+var player;
+
+window.onload = function() {
+canvas = document.getElementById("gamescreen")
+ctx = canvas.getContext("2d");
+
+
+player = new Player(100, 400);
+
+gameLoop = setInterval(step, 1000/40);
+
+}
+
+function step() {
+    console.log("step");
+}
---
+++ JAVA/player.js
@@ -0,0 +1,14 @@
+function Player(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 50;
+    this.height = 100;
+
+    this.step = function() {
+
+    }
+
+    this.draw = function() {
+        
+    }
+}
